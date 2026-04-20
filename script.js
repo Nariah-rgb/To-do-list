f96b37eef4246f63db62217a78ada3b3e4b1aa7c
Nariah <527nhenderson@frhsd.com>
2026-04-20 10:36:32 -0400

    Add initial HTML structure, JavaScript functionality, and CSS file for To-Do List application

---
+++ script.js
@@ -0,0 +1,40 @@
+let tasks = [] //empty array to store tasks
+
+document.getElementById('addTaskBtn').addEventListener('click', function () {
+    //get the value from input field
+    let taskInput = document.getElementById('taskInput').value;
+    //check if input is empty
+    if(taskInput) {
+        //add new tasks to task array
+        tasks.push(taskInput)
+        //clear input field value
+        document.getElementById('taskInput').value = '';
+        //update task list display
+        displayTasks()
+    };
+});
+
+function displayTasks() {
+    //select task list in the HTMl
+    let taskList = document.getElementById('taskList');
+    //clear the existing HTML list
+    taskList.innerHTML = '';
+    //loop through each task in the array and create a list item for each
+    tasks.forEach((task, index) => {
+        //create <li> element for each task
+        let li = document.createElement('li');
+        //add styling
+        li.classList.add(
+            'list-group-item',
+            'd-flex',
+            'justify-content-center',
+            'align-items-center'
+        )
+        //set the inner HTML of the LI with a task and remove button
+        li.innerHTML = `${task} <button class= 'btn btn-success btn-sm' onclick='removeTask(${index})'>✓</button>` 
+        //append the new task list to the HTML
+        taskList.appendChild(li)
+    });
+
+
+};
